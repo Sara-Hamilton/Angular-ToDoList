92357b1121b08ab4a0b4100ddb7ba133873c436a
Sara Hamilton <sara@indeterminate.net>
2018-03-28 15:33:26 -0700

    add ability to create a new task

--- to-do/src/app/new-task/new-task.component.ts
+++ to-do/src/app/new-task/new-task.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
   styleUrls: ['./new-task.component.css']
 })
-export class NewTaskComponent implements OnInit {
+export class NewTaskComponent  {
+  @Output() sendTask = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  submitForm(description: string, priority: string) {
+    let newTask: Task = new Task(description, parseInt(priority));
+    this.sendTask.emit(newTask);
   }
+
+  // constructor() { }
+  //
+  // ngOnInit() {
+  // }
 
 }
